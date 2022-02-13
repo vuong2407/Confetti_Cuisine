250cc5fc681e5008a89ad4e2b815ea246592dedd
vuong-nguyen-24072001 <vanvuong24072001@gmail.com>
2022-02-13 18:26:23 +0700

    fix: refactor hard coded connection string to Heroku config var

--- configs/db.js
+++ configs/db.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 async function connection() {
   try {
     await mongoose.connect(
-      process.env.MONGODB_URI,
-      "mongodb://localhost:27017/recipe_db",
+      process.env.MONGODB_URI || "mongodb://localhost:27017/recipe_db",
       {
         useNewUrlParser: true,
       }
